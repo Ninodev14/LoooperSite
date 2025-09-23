09cacb0756eea22d88c8d1d84e8517cba88e826e
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-09-23 16:54:52 +0200

    Update discord.js

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -1,13 +1,10 @@
-const querystring = require("querystring");
-
 exports.handler = async (event) => {
     if (event.httpMethod !== "POST") {
         return { statusCode: 405, body: "Méthode non autorisée" };
     }
-
-    console.log(event.body);
-
-    const data = querystring.parse(event.body);
+  console.log(event.body);
+    const body = JSON.parse(event.body);
+    const data = body.payload?.data || body.data || {};
 
     const objectifs = []
         .concat(data["objectif[]"] || [])
@@ -40,11 +37,14 @@ exports.handler = async (event) => {
         ]
     };
 
-    await fetch("https://discord.com/api/webhooks/1412352742088769596/pVZ586uEiVZs1fJ7rjyi6iqz-rzxGwyYeFX7M0PconGDQmMGvnaZAccgBIjZ8OJfqpq7", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(message)
-    });
+    await fetch("https://discord.com/api/webhooks/1412352742088769596/pVZ586uEiVZs1fJ7rjyi6iqz-rzxGwyYeFX7M0PconGDQmMGvnaZAccgBIjZ8OJfqpq7",
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(message)
+        }
+    );
 
     return { statusCode: 200, body: "Sent to Discord!" };
 };
+
