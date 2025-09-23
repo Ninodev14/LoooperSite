1374f64e9523434e093741a2295cb6a48183c925
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-09-23 19:03:28 +0200

    Update discord.js

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -2,30 +2,26 @@ exports.handler = async (event) => {
     if (event.httpMethod !== "POST") {
         return { statusCode: 405, body: "Méthode non autorisée" };
     }
-    console.log(event.body);
-    const body = JSON.parse(event.body);
 
-    // Priorité à human_fields, sinon data
-    const fields = body.human_fields || body.data || {};
+    const params = new URLSearchParams(event.body);
+    const fields = Object.fromEntries(params);
 
     const message = {
         content: "🚀 Nouvelle demande !",
         embeds: [
             {
-                title: `${fields["prenom"] || ""} ${fields["nom"] || ""}`,
+                title: `${fields.prenom || ""} ${fields.nom || ""}`,
                 fields: [
-                    { name: "Entreprise", value: fields["entreprise"] || "Non précisé", inline: true },
-                    { name: "Statut", value: fields["statut"] || "Non précisé", inline: true },
-                    { name: "Email", value: fields["email"] || "Non précisé", inline: true },
-                    { name: "Téléphone", value: fields["telephone"] || "Non précisé", inline: true },
-                    { name: "Message", value: fields["message"] || "Aucun" }
+                    { name: "Entreprise", value: fields.entreprise || "Non précisé", inline: true },
+                    { name: "Statut", value: fields.statut || "Non précisé", inline: true },
+                    { name: "Email", value: fields.email || "Non précisé", inline: true },
+                    { name: "Téléphone", value: fields.telephone || "Non précisé", inline: true },
+                    { name: "Message", value: fields.message || "Aucun" }
                 ],
                 color: 5814783
             }
         ]
     };
-
-
     await fetch("https://discord.com/api/webhooks/1412352742088769596/pVZ586uEiVZs1fJ7rjyi6iqz-rzxGwyYeFX7M0PconGDQmMGvnaZAccgBIjZ8OJfqpq7",
         {
             method: "POST",
