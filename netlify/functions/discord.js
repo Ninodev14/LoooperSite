6b47e75d88fc744fffdb6be57d1485ad1df57175
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-09-23 20:33:33 +0200

    undo discord form

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -7,20 +7,20 @@ exports.handler = async (event) => {
     const fields = Object.fromEntries(params);
 
     const message = {
-        content: "🚀 Nouvelle demande !",
-        embeds: [
-            {
-                title: `${fields.prenom || ""} ${fields.nom || ""}`,
-                fields: [
-                    { name: "Entreprise", value: fields.entreprise || "Non précisé", inline: true },
-                    { name: "Statut", value: fields.statut || "Non précisé", inline: true },
-                    { name: "Email", value: fields.email || "Non précisé", inline: true },
-                    { name: "Téléphone", value: fields.telephone || "Non précisé", inline: true },
-                    { name: "Message", value: fields.message || "Aucun" }
-                ],
-                color: 5814783
-            }
-        ]
+        content: "🚀 Nouvelle demande par mail ! Du site https://loooper.netlify.app",
+        //embeds: [
+        //    {
+        //       title: `${fields.prenom || ""} ${fields.nom || ""}`,
+        //       fields: [
+        //            { name: "Entreprise", value: fields.entreprise || "Non précisé", inline: true },
+        //           { name: "Statut", value: fields.statut || "Non précisé", inline: true },
+        //           { name: "Email", value: fields.email || "Non précisé", inline: true },
+        //            { name: "Téléphone", value: fields.telephone || "Non précisé", inline: true },
+        //            { name: "Message", value: fields.message || "Aucun" }
+        //         ],
+        //        color: 5814783
+        //    }
+        //  ]
     };
     await fetch("https://discord.com/api/webhooks/1412352742088769596/pVZ586uEiVZs1fJ7rjyi6iqz-rzxGwyYeFX7M0PconGDQmMGvnaZAccgBIjZ8OJfqpq7",
         {
