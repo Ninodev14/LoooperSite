5eea549b685a614a30cdadf02036425f5ad08343
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-09-23 18:27:49 +0200

    test

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -12,17 +12,18 @@ exports.handler = async (event) => {
             {
                 title: `${humanFields["prenom"] || ""} ${humanFields["nom"] || ""}`,
                 fields: [
-                    { name: "entreprise", value: humanFields["entreprise"] || "Non précisé", inline: true },
-                    { name: "statut", value: humanFields["statut"] || "Non précisé", inline: true },
-                    { name: "email", value: humanFields["email"] || "Non précisé", inline: true },
-                    { name: "telephone", value: humanFields["telephone"] || "Non précisé", inline: true },
-                    { name: "message", value: humanFields["message"] || "Aucun" }
+                    { name: "Entreprise", value: humanFields["entreprise"] || "Non précisé", inline: true },
+                    { name: "Statut", value: humanFields["statut"] || "Non précisé", inline: true },
+                    { name: "Email", value: humanFields["email"] || "Non précisé", inline: true },
+                    { name: "Téléphone", value: humanFields["telephone"] || "Non précisé", inline: true },
+                    { name: "Message", value: humanFields["message"] || "Aucun" }
                 ],
                 color: 5814783
             }
         ]
-    };
- 
+    }
+
+
     await fetch("https://discord.com/api/webhooks/1412352742088769596/pVZ586uEiVZs1fJ7rjyi6iqz-rzxGwyYeFX7M0PconGDQmMGvnaZAccgBIjZ8OJfqpq7",
         {
             method: "POST",
