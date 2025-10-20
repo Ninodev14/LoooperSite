bb37ba6c5e1c94c11fa3f6084be492524df021db
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-10-20 09:35:53 +0200

    add

--- src/js/3DEffect.js
+++ src/js/3DEffect.js
@@ -15,7 +15,6 @@ cards.forEach((card) => {
       const centerX = rect.width / 2;
       const centerY = rect.height / 2;
 
-      // inversion verticale + lissage
       let rotateX = -((y - centerY) / (centerY * 2)) * 50;
       let rotateY = ((x - centerX) / (centerX * 2)) * 50;
 
@@ -30,35 +29,40 @@ cards.forEach((card) => {
       if (card.classList.contains("flipped")) return;
       card.style.transition = "transform 0.2s ease-out";
       card.style.transform = "rotateX(0) rotateY(0)";
-      setTimeout(() => {
-        card.style.transition = "";
-      }, 200);
+      setTimeout(() => (card.style.transition = ""), 200);
     });
   } else {
     // Effet Mobile (touch)
+    let startX = 0;
+    let startY = 0;
+
+    card.addEventListener("touchstart", (e) => {
+      const touch = e.touches[0];
+      startX = touch.clientX;
+      startY = touch.clientY;
+    });
+
     card.addEventListener(
       "touchmove",
       (e) => {
         if (card.classList.contains("flipped")) return;
-        e.preventDefault();
 
         const touch = e.touches[0];
-        const rect = card.getBoundingClientRect();
-        const x = touch.clientX - rect.left;
-        const y = touch.clientY - rect.top;
+        const deltaX = Math.abs(touch.clientX - startX);
+        const deltaY = Math.abs(touch.clientY - startY);
+        if (deltaX > deltaY) {
+          e.preventDefault();
 
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
+          const rect = card.getBoundingClientRect();
+          const x = touch.clientX - rect.left;
+          const centerX = rect.width / 2;
 
-        // inversion verticale + lissage
-        let rotateX = -((y - centerY) / (centerY * 2)) * 50;
-        let rotateY = ((x - centerX) / (centerX * 2)) * 50;
+          let rotateY = ((x - centerX) / (centerX * 2)) * 50;
+          const maxAngle = 50;
+          rotateY = Math.max(-maxAngle, Math.min(maxAngle, rotateY));
 
-        const maxAngle = 70;
-        rotateX = Math.max(-maxAngle, Math.min(maxAngle, rotateX));
-        rotateY = Math.max(-maxAngle, Math.min(maxAngle, rotateY));
-
-        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.06)`;
+          card.style.transform = `rotateY(${rotateY}deg) scale(1.06)`;
+        }
       },
       { passive: false }
     );
@@ -66,10 +70,8 @@ cards.forEach((card) => {
     card.addEventListener("touchend", () => {
       if (card.classList.contains("flipped")) return;
       card.style.transition = "transform 0.2s ease-out";
-      card.style.transform = "rotateX(0) rotateY(0)";
-      setTimeout(() => {
-        card.style.transition = "";
-      }, 200);
+      card.style.transform = "rotateY(0)";
+      setTimeout(() => (card.style.transition = ""), 200);
     });
   }
 });
