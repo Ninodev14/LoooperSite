9a0f16655a831a92d349ccc9d9979d2b6ac6a7cd
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-10-31 11:42:59 +0100

    save card

--- src/js/flipCard.js
+++ src/js/flipCard.js
@@ -3,3 +3,25 @@ document.querySelectorAll('.protagonistes-card').forEach(card => {
     card.classList.toggle('flipped');
   });
 });
+function adjustCardsHeight() {
+    const cards = document.querySelectorAll('.protagonistes-card');
+    let maxHeight = 0;
+
+    // trouver la hauteur la plus grande parmi toutes les cartes
+    cards.forEach(card => {
+        const front = card.querySelector('.protagonistes-card-front');
+        const back = card.querySelector('.protagonistes-card-back');
+        const cardMaxHeight = Math.max(front.scrollHeight, back.scrollHeight);
+        if (cardMaxHeight > maxHeight) {
+            maxHeight = cardMaxHeight;
+        }
+    });
+
+    // appliquer la hauteur maximale à toutes les cartes
+    cards.forEach(card => {
+        card.style.height = maxHeight + 'px';
+    });
+}
+
+window.addEventListener('load', adjustCardsHeight);
+window.addEventListener('resize', adjustCardsHeight);
