63717b307a391db29361814b5626627e20b19b26
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-11-28 09:18:33 +0100

    fix load

--- src/js/flipCard.js
+++ src/js/flipCard.js
@@ -4,22 +4,25 @@ document.querySelectorAll('.protagonistes-card').forEach(card => {
   });
 });
 function adjustCardsHeight() {
-    const cards = document.querySelectorAll('.protagonistes-card');
-    let maxHeight = 0;
+  const cards = document.querySelectorAll('.protagonistes-card');
+  let maxHeight = 0;
 
-    cards.forEach(card => {
-        const front = card.querySelector('.protagonistes-card-front');
-        const back = card.querySelector('.protagonistes-card-back');
-        const cardMaxHeight = Math.max(front.scrollHeight, back.scrollHeight);
-        if (cardMaxHeight > maxHeight) {
-            maxHeight = cardMaxHeight;
-        }
-    });
+  cards.forEach(card => {
+    const front = card.querySelector('.protagonistes-card-front');
+    const back = card.querySelector('.protagonistes-card-back');
+    const cardMaxHeight = Math.max(front.scrollHeight, back.scrollHeight);
+    if (cardMaxHeight > maxHeight) {
+      maxHeight = cardMaxHeight;
+    }
+  });
 
-    cards.forEach(card => {
-        card.style.height = maxHeight + 'px';
-    });
+  cards.forEach(card => {
+    card.style.height = maxHeight + 'px';
+  });
 }
 
-window.addEventListener('load', adjustCardsHeight);
+setTimeout(() => {
+  window.addEventListener('load', adjustCardsHeight);
+}, 1000);
+
 window.addEventListener('resize', adjustCardsHeight);
