8dcfd77f91430f15b58db2f190b52ed5a6edeb55
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2026-08-19 11:27:25 +0200

    add db

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+// src/content/config.ts
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = { blog };
